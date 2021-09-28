902d93bb1813885d2fc376177f3a084025f40a67
Noor Kaur <81586152+Noor-Kaur@users.noreply.github.com>
2021-09-28 18:11:59 +0530

    Add files via upload
    creating a pattern using JavaScript

---
+++ pattern1.js
@@ -0,0 +1,23 @@
+//print the following pattern
+// *
+// *       *
+// *       *       *
+// *       *       *       *
+// *       *       *       *       *
+
+let args = process.argv;
+let n = parseInt(args[2]);
+
+for(let i=0; i<n; i++)
+{
+    let line  = "";
+    for(let j=0; j<=i; j++)
+    {
+        line = line + "*\t";
+    }
+    console.log(line);
+}
+
+//Run on terminal
+//npm install node
+//node pattern1.js 5
